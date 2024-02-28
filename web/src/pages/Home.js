b3bcb80f64821aae306727e754d5a9f3554439af
VM-Thuan-2003 <vmthuan16052003@gmail.com>
2024-02-28 13:36:14 +0700

    dev update feature home page

--- web/src/pages/Home.js
+++ web/src/pages/Home.js
@@ -1,9 +1,138 @@
-import React from 'react'
-
+import React from "react";
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className=" d-flex justify-content-center flex-column align-items-center">
+      <div className="">Title</div>
+      <div className="">Subtitle</div>
+      <div
+        id="carouselExampleCaptions"
+        className="carousel slide w-50 m-w-75"
+        data-bs-ride="carousel"
+      >
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to={0}
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          />
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to={1}
+            aria-label="Slide 2"
+          />
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to={2}
+            aria-label="Slide 3"
+          />
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to={3}
+            aria-label="Slide 4"
+          />
+          <button
+            type="button"
+            data-bs-target="#carouselExampleCaptions"
+            data-bs-slide-to={4}
+            aria-label="Slide 5"
+          />
+        </div>
+        <div className="carousel-inner">
+          <div className="carousel-item active">
+            <img
+              src={require("../static/imgs/BACKGROUND_1.jpeg")}
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>
+                Some representative placeholder content for the first slide.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img
+              src={require("../static/imgs/BACKGROUND_2.jpeg")}
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>
+                Some representative placeholder content for the second slide.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img
+              src={require("../static/imgs/BACKGROUND_3.jpeg")}
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>
+                Some representative placeholder content for the third slide.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img
+              src={require("../static/imgs/BACKGROUND_4.jpeg")}
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>four slide label</h5>
+              <p>
+                Some representative placeholder content for the third slide.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item">
+            <img
+              src={require("../static/imgs/BACKGROUND_5.jpeg")}
+              className="d-block w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>five slide label</h5>
+              <p>
+                Some representative placeholder content for the third slide.
+              </p>
+            </div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="prev"
+        >
+          <span className="carousel-control-prev-icon" aria-hidden="true" />
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carouselExampleCaptions"
+          data-bs-slide="next"
+        >
+          <span className="carousel-control-next-icon" aria-hidden="true" />
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+      <div className="">Group name</div>
+      <div className="">Slogan</div>
+    </div>
+  );
+};
 
-export default Home
+export default Home;
